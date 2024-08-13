b9be1fa3a01e7ef9f1adc1bf7a26e995f07e293e
Zahra Roshani <zahraroshani973@gmail.com>
2024-08-13 20:17:40 +0330

    adding another algorithm

--- Time analysis of algorithms/countingTriangle2.py
+++ Time analysis of algorithms/countingTriangle2.py
@@ -7,3 +7,17 @@ for i in range(1, n + 1):
         if i + j > k and k >= j:
             answer += 1
 print(answer)
+
+
+# second approach: a+b+c=n -> a+b = n-c -> a+b>c -> n-c>c -> n/2>c
+
+n = int(input())
+n1 = n//2+1
+ans = 0
+
+for a in range(n1):
+    for b in range(a, n1):
+        c = n-a-b
+        if a+b+c == n and a+b > c:
+            ans += 1
+print(ans)
