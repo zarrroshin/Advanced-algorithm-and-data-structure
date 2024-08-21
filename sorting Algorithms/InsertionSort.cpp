6a40477cf6289b9ec3820d1c61a9e709a2bc73f3
Zahra Roshani <zahraroshani973@gmail.com>
2024-08-21 18:49:12 +0330

    insertion sort algorithm added

---
+++ sorting Algorithms/InsertionSort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sort(int a[], int n)
+{
+    if (n == 0)
+        return;
+    sort(a, n - 1);
+    int p = n - 1;
+    while (p > 0 && a[p] < a[p - 1])
+    {
+        swap(a[p], a[p - 1]);
+        p--;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a, n);
+    for (int i = 0; i < n; i++)
+        cout << a[i] << ' ';
+    cout << endl;
+}
